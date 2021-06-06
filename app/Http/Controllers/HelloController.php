b2d5ff58a5898f7ce85e0a1a1160094791756962
YukiYamada <cat.of.attic@gmail.com>
2021-06-06 10:37:31 +0900

    [end] 3-4

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -8,7 +8,11 @@ use Illuminate\Http\Response;
 class HelloController extends Controller
 {
     public function index() {
-        $data = ['one', 'two', 'three', 'four', 'five'];
+        $data = [
+            ['name' => '山田太郎', 'mail' => 'taro@yamada' ],
+            ['name' => '田中花子', 'mail' => 'hanako@tanaka' ],
+            ['name' => '鈴木早智子', 'mail' => 'sachiko@happy' ],
+        ];
         return view('hello.index', ['data' => $data]);
     }
 
